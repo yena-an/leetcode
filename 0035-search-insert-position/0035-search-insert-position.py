6b3cecf457288fb008e6423812650d78b4e3c524
Yena An <105553993+yena-an@users.noreply.github.com>
2026-07-23 14:48:09 +0900

    Time: 0 ms (100%), Space: 13 MB (29.19%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        start = 0
+        end = len(nums) - 1
+        while start <= end:
+            mid = (start + end) // 2
+            if nums[mid] < target:
+                start = mid + 1
+            elif nums[mid] > target:
+                end = mid - 1
+            else:
+                return mid
+            
+        return start
