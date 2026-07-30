948e8a5ecab9f89a7e1d75d9a358953887294b82
Yena An <105553993+yena-an@users.noreply.github.com>
2026-07-31 08:47:11 +0900

    Time: 0 ms (100%), Space: 12.4 MB (20.03%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -4,15 +4,12 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        # def Fibonacci(n):
-        #     if n == 0 or n == 1:
-        #         return 1
-        #     return Fibonacci(n-1) + Fibonacci(n-2)
-         
-        # return Fibonacci(n)
-        a, b = 1, 1
+        start = 0
+        result = 1
 
-        for num in range(2, n + 1):
-            a, b = b, a + b
-            
-        return b
+        for _ in range(n):
+            i = result
+            result = start + i
+            start = i
+        
+        return result
